df432f7ae5260ef175462dcc8dcb730b1cf5b5aa
igorjung <igor.jung@outlook.com>
2019-12-20 17:07:25 -0300

    Updates

---
+++ precache-manifest.53f2c159e1951bdafe2843d98880e629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4e0cefc26b32e416866e531b8bbbfd",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "a4871ec7acb3ac3dce90",
+    "url": "/portifolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0051748055ec50ec5091",
+    "url": "/portifolio/static/js/2.ff5fad03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portifolio/static/js/2.ff5fad03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4871ec7acb3ac3dce90",
+    "url": "/portifolio/static/js/main.655bffdd.chunk.js"
+  },
+  {
+    "revision": "ac087b6b303b8080f6bf",
+    "url": "/portifolio/static/js/runtime-main.18b5140e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portifolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
